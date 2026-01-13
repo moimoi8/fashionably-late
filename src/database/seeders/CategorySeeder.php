20121a04efc62203ef92ca0c3b64a334486d551f
moimoi <trueworld311@gmail.com>
2026-01-13 14:01:28 +0900

    データベース設計と初期データ（シーダー）の作成完了

---
+++ src/database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            ['content' => '商品のお届けについて'],
+            ['content' => '商品の交換について'],
+            ['content' => '商品トラブル'],
+            ['content' => 'ショップへのお問い合わせ'],
+            ['content' => 'その他'],
+        ];
+
+        foreach ($items as $item) {
+            Category::create($item);
+        }
+    }
+}
